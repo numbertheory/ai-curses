038d384e1d31710d101d6d9c657aa0766a0f5738
JP Etcheber <jetcheber@gmail.com>
2023-03-19 15:00:07 -0700

    getting prompt working

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 #! /usr/bin/env python3
 from dashport.dash import Dashport
 from dashport.run import wrap
-from dashport.prompt import user_prompt
+from ai_curses import new_prompt
 
 
 def quit():
     exit(0)
 
 
+def ai_response(prompt):
+    return f"This was {len(prompt)} character(s) long."
+
+
 def dashport(stdscr):
     app = Dashport(stdscr, color_default=8)
-    app.layout("single_panel", border=False, scroll=True, height=app.rows - 1)
+    app.layout("single_panel", border=False, scroll=True, height=app.rows - 4)
+    app.addstr(">", x=0, y=app.rows - 3)
     app.commands = []
     while True:
         while True:
             app.user_prompt_position = 1
-            app.print(content="{}".format(app.current_command),
-                      x=0, y=app.rows - 2, panel="layout.0")
-            app.addstr(">", x=0, y=app.rows - 1)
-            command = user_prompt(app, x=2, y=app.rows - 1)
-            if command == "quit":
+            command = new_prompt.user_prompt(app, x=2, y=app.rows - 3,
+                                             height=2, width=app.cols)
+            response = f"AI> {ai_response(command)}"
+            app.panels["layout"][0].scroll(1)
+            app.print(content="Human> {}".format(app.current_command),
+                      x=0, y=app.rows - 4, panel="layout.0")
+            app.print(content="{}".format(response),
+                      x=0, y=app.rows - 3, panel="layout.0")
+            app.panels["layout"][0].scroll(1)
+            app.addstr(">", x=0, y=app.rows - 3)
+            if command == "quit" or command == "exit":
                 quit()
-            else:
-                app.panels["layout"][0].scroll(1)
         app.refresh()
 
 
